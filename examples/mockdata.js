d0bc68cc9b2dbef3ee56306f14b2d88bdfd9b43d
Varun Sharma <varun_sharma@live.co.uk>
2023-08-11 00:19:04 -0700

    feat: add new inputs and song data selects

---
+++ examples/mockdata.js
@@ -0,0 +1,21 @@
+const testMusicData = [
+	{
+		title: "Losing it",
+		artist: "FISHER",
+		albumCoverUrl:
+			"https://e.snmc.io/i/1200/s/48fe6b7f9a0c7cfd8b6b0ae3a688259c/7147933",
+		bpm: 124,
+		audioUrl:
+			"https://storage.googleapis.com/ad-auris-django-bucket/media/FISHER%20-%20Losing%20It.mp3",
+	},
+	{
+		title: "Stop it",
+		albumCoverUrl:
+			"https://e.snmc.io/i/1200/s/48fe6b7f9a0c7cfd8b6b0ae3a688259c/7147933",
+
+		artist: "FISHER",
+		bpm: 125,
+		audioUrl:
+			"https://storage.googleapis.com/ad-auris-django-bucket/media/Fisher_-_Stop_It_Original_Mix%5BMp3Converter.n.mp3",
+	},
+];
